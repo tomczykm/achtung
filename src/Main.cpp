bcdbb7125cb22b1f502e8aab9301d202c66181d0
MT <tomczykmk94@gmail.com>
2019-11-30 11:29:47 +0100

    Include SFML into project

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <memory>
 
+#include <SFML/Graphics.hpp>
+
 #include "Application.hpp"
 #include "Log.hpp"
 
@@ -11,11 +13,29 @@ std::unique_ptr<std::FILE, FCloseDeleter> logFile;
 
 int main(int argc, char **argv) {
     print::detail::logFile.reset(std::fopen("event.log", "w"));
-    Application app;
-    if (!app.init()) {
-        print::error("Initialization failed");
-        return -1;
-    }
+    // Application app;
+    // if (!app.init()) {
+    //     print::error("Initialization failed");
+    //     return -1;
+    // }
 
-    return app.run();
+    // return app.run();
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
 }
